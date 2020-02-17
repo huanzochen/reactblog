45cacd4a00986a2adfe69c9646bf71138c464772
tommy <tommy.huang@wavenet.com.tw>
2020-02-17 11:32:23 +0800

    畫面

--- react_blog/src/view/Homepage.js
+++ react_blog/src/view/Homepage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import clsx from "clsx";
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Drawer from '@material-ui/core/Drawer';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
@@ -11,11 +12,48 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import { IconButton } from '@material-ui/core';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
-const useStyles = makeStyles( () => ({
+const drawerWidth = 240;
+
+const useStyles = makeStyles( (theme) => ({
     root: {
         display: 'flex',
     },
+    list: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        marginLeft: -drawerWidth
+    },
+    contentShift: {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen
+        }),
+        marginLeft: 0
+    }
     
 })
 
@@ -26,59 +64,59 @@ const useStyles = makeStyles( () => ({
 
 const Homepage = () => {
     const classes = useStyles();
-    const [state, setState] = React.useState({
-        left: true,
-    });
-    const toggleDrawer = (side, open) => event => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-          return;
-        }
-    
-        setState({ ...state, [side]: open });
+    const [open, setOpen] = React.useState(true);
+    const handleDrawerClose = () => {
+        setOpen(false);
     };
-
-
-    const sideList = side => (
-        <div
-          className={classes.list}
-          role="presentation"
-          onClick={toggleDrawer(side, false)}
-          onKeyDown={toggleDrawer(side, false)}
-        >
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-    );
-
-
 
     return (
         <div>
             <CssBaseline />
             <Drawer
-                open={state.left}
-                onClose={toggleDrawer('left', true)}
+                open = {open}
+                className = {classes.drawer}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+                anchor="left"
+                variant="persistent"
             >
-                {sideList('left')}
-                <Typography variant="subtitle2" gutterBottom>
-                    subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-                </Typography>
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                    ))}
+                </List>
             </Drawer>
+            <main
+                className={
+                    clsx({ [classes.content] : true}, {
+                        [classes.contentShift]: open
+                    })}
+            >
+                <div className={classes.drawerHeader} />
+                <Typography paragraph>
+                    文字 goes here文字 goes here文字 goes here文字 goes here文字 goes here文字 goes here文字 goes here
+                </Typography >    
+
+            </main>
         </div>
 
 
