3ac90fd1302fc0ee9de8b7aaa74f727cff828d19
g <fd>
2020-02-24 00:54:29 +0800

    新增 測試

--- momo-egg/test/controller/blog.test.js
+++ momo-egg/test/controller/blog.test.js
@@ -3,13 +3,40 @@ const { app, mock, assert } = require('egg-mock/bootstrap');
 describe('test/controller/blog.test.js', () => {
     // test cases
     describe('GET /', () => {
-        it('should status 200 and get the body', () => {
-          // 对 app 发起 `GET /` 请求
-          return app.httpRequest()
-            .get('/api/article')
-            .expect(200) // 期望返回 status 200
-        });
+      it('文章列表', async () => {
+        // 对 app 发起 `GET /` 请求
+        await app.httpRequest()
+          .get('/api/article')
+          .expect(200) // 期望返回 status 200
+          .then(response => {
+            assert(response.body[0].id, 'string');
+            assert(response.body[0].act_name, 'string');
+            assert(response.body[0].title, 'string');
+            assert(response.body[0].content, 'string');
+            assert(response.body[0].edit_time, 'string');
+            assert(response.body[0].create_time, 'string');
+          })
       });
+    });
+
+
+    describe('POST /', () => {
+      it('新增文章', async () => {
+        // 对 app 发起 `GET /` 请求
+        await app.httpRequest()
+          .post('/api/article/new')
+          .send({
+            article:{
+              act_name:'testonly',
+              title:'testarticle',
+              content: 'testarticlecontentcontent'
+            }
+          })
+          .then(response => {
+            assert(response, true);
+          })
+      });
+    });
 
 
 
