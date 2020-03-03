01cd9f7ead4568c402ad7d6c140ec8cf18829047
tommy <tommy.huang@wavenet.com.tw>
2020-03-03 18:29:36 +0800

    unit test 新增中(下班 commit)

---
+++ momo-egg/test/service/blog.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+const assert = require('assert');
+const mm = require('egg-mock');
+const { app, mock, assert } = require('egg/mock/bootstrap');
+
+describe('test/service/blog.test.js', () => {
+    const ctx = app.mockContext();
+    
+
+
+
+})
